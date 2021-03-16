e4f0c12dfbf6ca724e8be7d5cd0161e2988002ca
sourcebd <nativetube71@gmail.com>
2021-03-16 19:19:02 +0600

    Customer Part done by Mahmud, Nafi [17-33793-1]

--- laratest/resources/views/layout/nav_bar.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout.main')
-
-
-@section('nav_bar')
-<a href="{{route('home.Ccreate')}}">Create User</a> |
-<a href="{{route('home.Cuserlist')}}">View User List</a> |
-<a href="{{route('logout.index')}}">Logout</a>
-@endsection
