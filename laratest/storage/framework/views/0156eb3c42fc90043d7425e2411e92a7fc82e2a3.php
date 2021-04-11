19f06d2057f18150ac2b8ae30ee8b388a59eae25
sourcebd <nativetube71@gmail.com>
2021-04-12 00:47:40 +0600

    Full Merging Done Contributed by 17-33793-1 & 18-36673-1

--- laratest/storage/framework/views/0156eb3c42fc90043d7425e2411e92a7fc82e2a3.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Not Found')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('message', __('Not Found')); ?>
-
-<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\emon5\OneDrive\Desktop\last semi\ATP3\sir\teaching-laravel-framework-spring-2020-2021-sec-b-main\laratest\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
