ea2347ab49826c8b930b91a97f599c5627167d33
sourcebd <nativetube71@gmail.com>
2021-04-11 19:50:50 +0600

    Some Changes

--- laratest/storage/framework/views/5135c2dc76231a2564af6f397128792367cb1277.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('nav_bar'); ?>
-<a href="<?php echo e(route('home.Ccreate')); ?>">Create User</a> |
-<a href="<?php echo e(route('home.Cuserlist')); ?>">View User List</a> |
-<a href="<?php echo e(route('logout.index')); ?>">Logout</a>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layout.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH F:\13th Semester\ATP 3\Laravel-Project\laratest\resources\views/layout/nav_bar.blade.php ENDPATH**/ ?>
--- laratest/storage/framework/views/dd48bcaecc6ba1e1191f2572f60467698e764b74.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('nav_bar'); ?>
-<a href="/home/create">Create user</a> |
-<a href="<?php echo e(route('home.userlist')); ?>">View user list</a> |
-<a href="/logout">logout</a>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layout.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH F:\13th Semester\ATP 3\Laravel-Project\Mahmud, Nafi\laratest\resources\views/layout/nav_bar.blade.php ENDPATH**/ ?>
